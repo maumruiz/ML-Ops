436cff76b17f0fb94f7eecf616df84668689c917
maumruiz <mau.mruiz@gmail.com>
2021-12-05 22:29:04 -0600

    Update model directory

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -1,26 +1,29 @@
 import prefect
 from prefect import task, Flow
 from prefect.tasks.jupyter import ExecuteNotebook
-from datetime import timedelta
 from prefect.schedules import Schedule
+from prefect.triggers import all_successful
 from prefect.schedules.clocks import DatesClock, CronClock
+
+from datetime import timedelta
 import pendulum
 
 from tasks.data_extraction import obtain_data
 
 logger = prefect.context.get("logger")
 
-@task(max_retries=3, retry_delay=timedelta(seconds=30))
+@task(max_retries=3, retry_delay=timedelta(seconds=30), trigger=all_successful)
 def dp_task():
     ExecuteNotebook(path='tasks/data_wrangling.ipynb', kernel_name='python3').run()
 
-@task(max_retries=3, retry_delay=timedelta(seconds=30))
+@task(max_retries=3, retry_delay=timedelta(seconds=30), trigger=all_successful)
 def pp_task():
     ExecuteNotebook(path='tasks/pre_processing.ipynb', kernel_name='python3').run()
 
-@task(max_retries=3, retry_delay=timedelta(seconds=30))
+@task(max_retries=3, retry_delay=timedelta(seconds=30), trigger=all_successful)
 def model_task():
     ExecuteNotebook(path='tasks/model.ipynb', kernel_name='python3').run()
+
 
 @task
 def data_extract():
@@ -28,9 +31,6 @@ def data_extract():
     obtain_data()
     logger.info("Datos obtenidos!")
 
-# scheduler = CronSchedule(
-#     cron='0 0 * * 0'
-# )
 
 scheduler = Schedule(
     clocks=[DatesClock([pendulum.now().add(seconds=10)]), CronClock("0 0 * * 0")],
@@ -38,8 +38,8 @@ scheduler = Schedule(
 
 with Flow("spike-pipeline", schedule=scheduler) as flow:
     data_extract()
-    dp_task()
-    pp_task()
-    model_task()
+    dp_task(upstream_tasks=[data_extract])
+    pp_task(upstream_tasks=[dp_task])
+    model_task(upstream_tasks=[pp_task])
 
 flow.run()
