26869f55cc8fa4c3e3b9f05ab8aae094a4be0349
maumruiz <mau.mruiz@gmail.com>
2021-12-06 21:32:41 -0600

    Set task dependencies and hide notebook input log

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -1,9 +1,10 @@
 import prefect
 from prefect import task, Flow
-from prefect.tasks.jupyter import ExecuteNotebook
 from prefect.schedules import Schedule
 from prefect.triggers import all_successful
 from prefect.schedules.clocks import DatesClock, CronClock
+
+from tasks.jupyter.jupyter import ExecuteNotebook
 
 from datetime import timedelta
 import pendulum
@@ -13,16 +14,16 @@ from tasks.data_extraction import obtain_data
 logger = prefect.context.get("logger")
 
 @task(max_retries=3, retry_delay=timedelta(seconds=30), trigger=all_successful)
-def dp_task():
-    ExecuteNotebook(path='tasks/data_wrangling.ipynb', kernel_name='python3').run()
+def data_wrangling(task_input):
+    ExecuteNotebook(path='tasks/data_wrangling.ipynb', output_path='saved/out_data_wrangling.ipynb', kernel_name='python3').run()
 
 @task(max_retries=3, retry_delay=timedelta(seconds=30), trigger=all_successful)
-def pp_task():
-    ExecuteNotebook(path='tasks/pre_processing.ipynb', kernel_name='python3').run()
+def pre_processing(task_input):
+    ExecuteNotebook(path='tasks/pre_processing.ipynb', output_path='saved/out_pre_processing.ipynb', kernel_name='python3').run()
 
 @task(max_retries=3, retry_delay=timedelta(seconds=30), trigger=all_successful)
-def model_task():
-    ExecuteNotebook(path='tasks/model.ipynb', kernel_name='python3').run()
+def model_training(task_input):
+    ExecuteNotebook(path='tasks/model_training.ipynb', output_path='saved/out_model_training.ipynb', kernel_name='python3').run()
 
 
 @task
@@ -37,9 +38,9 @@ scheduler = Schedule(
     )
 
 with Flow("spike-pipeline", schedule=scheduler) as flow:
-    data_extract()
-    dp_task(upstream_tasks=[data_extract])
-    pp_task(upstream_tasks=[dp_task])
-    model_task(upstream_tasks=[pp_task])
+    out1 = data_extract()
+    out2 = data_wrangling(task_input=out1)
+    out3 = pre_processing(task_input=out2)
+    out4 = model_training(task_input=out3)
 
 flow.run()
